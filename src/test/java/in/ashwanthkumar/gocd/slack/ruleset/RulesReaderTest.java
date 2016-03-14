c2004d71ba22dce7227e90962cb435319a620b5d
Juha Siponen <juha.siponen@vincit.fi>
2016-03-14 18:33:02 +0200

    Add tests for simple configurations
     * Minimal config with one pipeline
     * Minimal config without pipelines (Issue #23)
     * Minimal invalid config

--- src/test/java/in/ashwanthkumar/gocd/slack/ruleset/RulesReaderTest.java
+++ src/test/java/in/ashwanthkumar/gocd/slack/ruleset/RulesReaderTest.java
@@ -3,9 +3,7 @@ package in.ashwanthkumar.gocd.slack.ruleset;
 import in.ashwanthkumar.utils.collections.Sets;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class RulesReaderTest {
@@ -32,15 +30,45 @@ public class RulesReaderTest {
                 .setChannel("#gocd-build")
                 .setStatus(Sets.of(PipelineStatus.FAILED));
         assertThat(rules.getPipelineRules(), hasItem(pipelineRule2));
+
+        assertThat(rules.getPipelineListener(), notNullValue());
     }
 
     @Test
     public void shouldReadMinimalConfig() {
         Rules rules = RulesReader.read("test-config-minimal.conf");
+
+        assertThat(rules.isEnabled(), is(true));
+
+        assertThat(rules.getGoLogin(), is("someuser"));
+        assertThat(rules.getGoPassword(), is("somepassword"));
+        assertThat(rules.getGoServerHost(), is("http://localhost:8153/"));
+        assertThat(rules.getWebHookUrl(), is("https://hooks.slack.com/services/"));
+
+        assertThat(rules.getSlackChannel(), is("#build"));
+        assertThat(rules.getSlackDisplayName(), is("gocd-slack-bot"));
+        assertThat(rules.getSlackUserIcon(), is("http://example.com/slack-bot.png"));
+
+        // Default rules
+        assertThat(rules.getPipelineRules().size(), is(1));
+
+        PipelineRule pipelineRule = new PipelineRule()
+                .setNameRegex(".*")
+                .setStageRegex(".*")
+                .setChannel("#build")
+                .setStatus(Sets.of(PipelineStatus.CANCELLED, PipelineStatus.BROKEN, PipelineStatus.FAILED, PipelineStatus.FIXED));
+        assertThat(rules.getPipelineRules(), hasItem(pipelineRule));
+
+        assertThat(rules.getPipelineListener(), notNullValue());
+    }
+
+    @Test
+    public void shouldReadMinimalConfigWithPipeline() {
+        Rules rules = RulesReader.read("test-config-minimal-with-pipeline.conf");
         assertThat(rules.isEnabled(), is(true));
         assertThat(rules.getSlackChannel(), nullValue());
         assertThat(rules.getGoServerHost(), is("https://go-instance:8153/"));
-        assertThat(rules.getWebHookUrl(), is("http://slack-instance.net/"));
+        assertThat(rules.getWebHookUrl(), is("https://hooks.slack.com/services/"));
         assertThat(rules.getPipelineRules().size(), is(1));
 
         PipelineRule pipelineRule = new PipelineRule()
@@ -49,6 +77,14 @@ public class RulesReaderTest {
                 .setChannel("#foo")
                 .setStatus(Sets.of(PipelineStatus.FAILED));
         assertThat(rules.getPipelineRules(), hasItem(pipelineRule));
+
+        assertThat(rules.getPipelineListener(), notNullValue());
     }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionIfConfigInvalid() {
+        RulesReader.read("test-config-invalid.conf");
+    }
+
 
 }
