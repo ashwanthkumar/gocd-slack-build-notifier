f7c02a9e2e6a5cfa0a61cd88057593e69c4aceda
binoysh <binoysh@payoneer.com>
2023-10-11 11:15:23 +0200

    Changes as per comments
    Added .* for label match and suggested changes

--- src/test/java/in/ashwanthkumar/gocd/slack/ruleset/RulesTest.java
+++ src/test/java/in/ashwanthkumar/gocd/slack/ruleset/RulesTest.java
@@ -18,22 +18,22 @@ public class RulesTest {
         Rules rules = new Rules();
 
         rules.setPipelineRules(Arrays.asList(
-                pipelineRule("pipeline1", "stage1", "ch1", "", statuses(PipelineStatus.BUILDING, PipelineStatus.FAILED)),
-                pipelineRule("pipeline1", "stage2", "ch2", "", statuses(PipelineStatus.FIXED, PipelineStatus.PASSED)),
-                pipelineRule("pipeline2", "stage2", "ch3", "", statuses(PipelineStatus.CANCELLED, PipelineStatus.BROKEN))
+                pipelineRule("pipeline1", "stage1", "ch1", ".*", statuses(PipelineStatus.BUILDING, PipelineStatus.FAILED)),
+                pipelineRule("pipeline1", "stage2", "ch2", ".*", statuses(PipelineStatus.FIXED, PipelineStatus.PASSED)),
+                pipelineRule("pipeline2", "stage2", "ch3", ".*", statuses(PipelineStatus.CANCELLED, PipelineStatus.BROKEN))
         ));
 
-        List<PipelineRule> foundRules1 = rules.find("pipeline1", "stage1", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules1 = rules.find("pipeline1", "stage1", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules1.size(), is(1));
         assertThat(foundRules1.get(0).getNameRegex(), is("pipeline1"));
         assertThat(foundRules1.get(0).getStageRegex(), is("stage1"));
 
-        List<PipelineRule> foundRules2 = rules.find("pipeline2", "stage2", "ci", "", Status.Cancelled.getStatus());
+        List<PipelineRule> foundRules2 = rules.find("pipeline2", "stage2", "ci", ".*", Status.Cancelled.getStatus());
         assertThat(foundRules2.size(), is(1));
         assertThat(foundRules2.get(0).getNameRegex(), is("pipeline2"));
         assertThat(foundRules2.get(0).getStageRegex(), is("stage2"));
 
-        List<PipelineRule> foundRules3 = rules.find("pipeline2", "stage2", "ci", "", Status.Passed.getStatus());
+        List<PipelineRule> foundRules3 = rules.find("pipeline2", "stage2", "ci", ".*", Status.Passed.getStatus());
         assertThat(foundRules3.size(), is(0));
     }
 
@@ -42,31 +42,31 @@ public class RulesTest {
         Rules rules = new Rules();
 
         rules.setPipelineRules(Arrays.asList(
-                pipelineRule("[a-z]*", "[a-z]*", "ch1", "", statuses(PipelineStatus.BUILDING)),
-                pipelineRule("\\d*", "\\d*", "ch2", "", statuses(PipelineStatus.BUILDING)),
-                pipelineRule("\\d*", "\\d*", "ch3", "", statuses(PipelineStatus.PASSED)),
-                pipelineRule("\\d*", "[a-z]*", "ch4", "", statuses(PipelineStatus.BUILDING))
+                pipelineRule("[a-z]*", "[a-z]*", "ch1", ".*", statuses(PipelineStatus.BUILDING)),
+                pipelineRule("\\d*", "\\d*", "ch2", ".*", statuses(PipelineStatus.BUILDING)),
+                pipelineRule("\\d*", "\\d*", "ch3", ".*", statuses(PipelineStatus.PASSED)),
+                pipelineRule("\\d*", "[a-z]*", "ch4", ".*", statuses(PipelineStatus.BUILDING))
         ));
 
-        List<PipelineRule> foundRules1 = rules.find("abc", "efg", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules1 = rules.find("abc", "efg", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules1.size(), is(1));
         assertThat(foundRules1.get(0).getNameRegex(), is("[a-z]*"));
         assertThat(foundRules1.get(0).getStageRegex(), is("[a-z]*"));
         assertThat(foundRules1.get(0).getChannel(), is("ch1"));
 
-        List<PipelineRule> foundRules2 = rules.find("123", "456", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules2 = rules.find("123", "456", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules2.size(), is(1));
         assertThat(foundRules2.get(0).getNameRegex(), is("\\d*"));
         assertThat(foundRules2.get(0).getStageRegex(), is("\\d*"));
         assertThat(foundRules2.get(0).getChannel(), is("ch2"));
 
-        List<PipelineRule> foundRules3 = rules.find("123", "456", "ci", "", Status.Passed.getStatus());
+        List<PipelineRule> foundRules3 = rules.find("123", "456", "ci", ".*", Status.Passed.getStatus());
         assertThat(foundRules3.size(), is(1));
         assertThat(foundRules3.get(0).getNameRegex(), is("\\d*"));
         assertThat(foundRules3.get(0).getStageRegex(), is("\\d*"));
         assertThat(foundRules3.get(0).getChannel(), is("ch3"));
 
-        List<PipelineRule> foundRules4 = rules.find("pipeline1", "stage1", "ci", "", Status.Passed.getStatus());
+        List<PipelineRule> foundRules4 = rules.find("pipeline1", "stage1", "ci", ".*", Status.Passed.getStatus());
         assertThat(foundRules4.size(), is(0));
     }
 
@@ -76,20 +76,20 @@ public class RulesTest {
         rules.setProcessAllRules(true);
 
         rules.setPipelineRules(Arrays.asList(
-                pipelineRule("[a-z]*", "stage\\d+", "ch1", "", statuses(PipelineStatus.BUILDING)),
-                pipelineRule("[a-z]*", "stage2", "ch2", "", statuses(PipelineStatus.BUILDING))
+                pipelineRule("[a-z]*", "stage\\d+", "ch1", ".*", statuses(PipelineStatus.BUILDING)),
+                pipelineRule("[a-z]*", "stage2", "ch2", ".*", statuses(PipelineStatus.BUILDING))
         ));
 
-        List<PipelineRule> foundRules1 = rules.find("abc", "stage1", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules1 = rules.find("abc", "stage1", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules1.size(), is(1));
         assertThat(foundRules1.get(0).getChannel(), is("ch1"));
 
-        List<PipelineRule> foundRules2 = rules.find("abc", "stage2", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules2 = rules.find("abc", "stage2", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules2.size(), is(2));
         assertThat(foundRules2.get(0).getChannel(), is("ch1"));
         assertThat(foundRules2.get(1).getChannel(), is("ch2"));
 
-        List<PipelineRule> foundRules3 = rules.find("abc1", "stage2", "ci", "", Status.Building.getStatus());
+        List<PipelineRule> foundRules3 = rules.find("abc1", "stage2", "ci", ".*", Status.Building.getStatus());
         assertThat(foundRules3.size(), is(0));
     }
 
@@ -101,14 +101,14 @@ public class RulesTest {
                 pipelineRule("p1", "s1", "ch1", "", statuses(PipelineStatus.ALL))
         ));
 
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Building.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Broken.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Cancelled.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Failed.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Failing.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Fixed.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Passed.getStatus()).size(), is(1));
-        assertThat(rules.find("p1", "s1", "ci", "", Status.Unknown.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Building.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Broken.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Cancelled.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Failed.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Failing.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Fixed.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Passed.getStatus()).size(), is(1));
+        assertThat(rules.find("p1", "s1", "ci", ".*", Status.Unknown.getStatus()).size(), is(1));
     }
 
     @Test
